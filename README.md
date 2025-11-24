4760f831b97e1136329f2f79895af37836578610
Oscar Holt <oscar.holt99@gmail.com>
2025-11-24 18:57:31 +1300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Working on some Linguistics stuff
+
+TODO:
+* Build Web Scraper
---
+++ scraper.py
@@ -0,0 +1,7 @@
+# Scrape this: https://imsdb.com/
+# Ideally we want have directories for each genre
+# Would be nice if it auto-generated the cat_file for corpus as well
+def main():
+    print("Hello World")
+
+main()
